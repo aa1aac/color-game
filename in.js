e607927c6863d1f3673250715e15128269e3d2f5
aa11ac <ach.aayush.111@gmail.com>
2018-04-08 17:34:29 +0545

    update

--- in.js
+++ in.js
@@ -1,16 +1,10 @@
-var colors=[
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
+var colors= genertaterandomcolors(6);
 var pickedColor=pickColor();
 var squares=document.querySelectorAll(".squares");
 var colorDisplay=document.querySelector("#colorDisplay");
 colorDisplay.textContent=pickedColor;
 var messageDisplay=document.querySelector("#message");
+var h1=document.querySelector("h1");
 for (var i=0;i<squares.length;i++){
     // add initial colors
     squares[i].style.backgroundColor=colors[i];
@@ -20,8 +14,10 @@ for (var i=0;i<squares.length;i++){
         var clickedColor=this.style.backgroundColor;
         // compare the clicked color
         if (clickedColor===pickedColor){
+
             messageDisplay.textContent="CORRECT";
             changeColor(clickedColor);
+            h1.style.backgroundColor=clickedColor;
         }
         else{
          this.style.backgroundColor="#232323";
@@ -41,4 +37,24 @@ for (var i=0;i<squares.length;i++){
  function pickColor(){
      var random=Math.floor(Math.random()*colors.length);
      return colors[random];
+ }
+ function genertaterandomcolors(num){
+     // make an array
+     var arr=[];
+     // add num random color to array
+      for (var i=0;i<num;i++){
+       //get random color and push into array
+       arr.push(randomColor());
+      };
+     // return the array
+     return arr;
+ }
+ function randomColor(){
+     // pick red
+     var r=Math.floor(Math.random()*256);
+     //pick green
+     var g=Math.floor(Math.random()*256);
+     // pick blue
+     var b=Math.floor(Math.random()*256);
+     return ("rgb("+r+", "+g+", "+b+")");
  }
